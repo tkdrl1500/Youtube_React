125578fc9b24bcfff1c7c924d93217255751019e
lsg <tkdrl1500@gmail.com>
2021-11-24 16:54:21 +0900

    [Add] show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>Hello</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&maxResults=25&chart=mostpopular&key=AIzaSyAJpYdpeaxtwoC85cHy8-n4h2qNS4zv3TQ',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;

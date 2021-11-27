4677b79edcd42218137be2d98af6c13c91c2eddf
lsg <tkdrl1500@gmail.com>
2021-11-27 21:15:58 +0900

    [Add] SearchBar UI 구현 및 검색기능 구현

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,27 @@
 import { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchBar from './components/searchBar/searchBar';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const onSearch = (query) => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyAJpYdpeaxtwoC85cHy8-n4h2qNS4zv3TQ`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log('error', error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -19,7 +37,12 @@ function App() {
       .then((result) => setVideos(result.items))
       .catch((error) => console.log('error', error));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchBar onSearch={onSearch} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/searchBar/searchBar.jsx
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react';
+import styles from './searchBar.module.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+const SearchBar = ({ onSearch }) => {
+  const inputRef = useRef();
+  const handleValue = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+  const onClick = () => {
+    handleValue();
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleValue();
+    }
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <span className={styles.img}>
+          <i className="fab fa-youtube"></i>
+        </span>
+        <h1>YOUTUBE</h1>
+      </div>
+      <input
+        onKeyPress={onKeyPress}
+        ref={inputRef}
+        className={styles.input}
+        type="search"
+        placeholder="Search..."
+      />
+      <button onClick={onClick} className={styles.btn} type="submit">
+        <i className="fas fa-search"></i>
+      </button>
+    </header>
+  );
+};
+
+export default SearchBar;

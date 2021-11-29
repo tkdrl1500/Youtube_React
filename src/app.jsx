e8fecc6007bbb9ff233098c909e1df85ec7cc717
lsg <tkdrl1500@gmail.com>
2021-11-29 15:25:13 +0900

    [Add] 비디오 선택 및 상세화면

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchBar from './components/searchBar/searchBar';
+import VideoDetail from './components/video_detail/video_detail';
 import VideoList from './components/video_list/video_list';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+
   const onSearch = (query) => {
     youtube
       .search(query) //
@@ -19,7 +26,20 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchBar onSearch={onSearch} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
+        </div>
+      </section>
     </div>
   );
 }
